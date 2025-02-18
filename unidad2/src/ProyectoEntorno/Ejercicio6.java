3a345cf8185fb9fe4b0d7899756ec72da28f1f19
Carnecontomate12 <fbarcru2210@g.educaand.es>
2025-02-18 12:40:34 +0100

    subida martes

---
+++ unidad2/src/ProyectoEntorno/Ejercicio6.java
@@ -0,0 +1,9 @@
+package ProyectoEntorno;
+
+public class Ejercicio6 {
+	  public static void main(String[] args) {
+	        String palabra = "escarabajo";
+	        int longitud = palabra.length();
+	        System.out.println("Longitud de la palabra: " + longitud);
+	}
+}
